4fde7ec164c2a575b15bc95de853db7cf7ff7999
kimuraYuki <kimuraYuki@192.168.10.106>
2015-05-02 11:12:30 +0900

    前回のテストの部分がうまく上がってなかったのでその部分の上げ直し

--- snake-camel/src/snakecamel/SnakeCamelUtil.java
+++ snake-camel/src/snakecamel/SnakeCamelUtil.java
@@ -5,7 +5,7 @@ public class SnakeCamelUtil {
 	public static String snakeToCamelcase(String snake_case) {
 		String[] words = snake_case.split("_");
 		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < words.length - 1; i++) {
+		for (int i = 0; i < words.length; i++) {
 			sb.append(capitalize(words[i]));
 		}
 		return new String(sb);
@@ -21,12 +21,12 @@ public class SnakeCamelUtil {
 				if (sb.length() > 0) {
 					sb.append("_");
 				}
-				sb.append(Character.toLowerCase(c));
+				//sb.append(Character.toLowerCase(c));
 				j = i;
 			}
 		}
 		sb.append(camelcase.substring(j));
-		return new String(sb);
+		return new String(sb).toLowerCase();
 	}
 	
 	static String capitalize(String s) {
@@ -36,11 +36,11 @@ public class SnakeCamelUtil {
 		return upperFirst + rest;
 	}
 
-	static String uncapitalize(String s) {
+/*	static String uncapitalize(String s) {
 		char first = s.charAt(0);
 		char lowerFirst = Character.toLowerCase(first);
 		String rest = s.substring(1);
 		return lowerFirst + rest;
-	}
+	}*/
 	
 }
